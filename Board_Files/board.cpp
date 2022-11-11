0821bf5f5d0f2e96f1ec05c938ff3ac263a5ca3d
cmatt6 <cmatt6@umbc.edu>
2022-11-11 11:49:17 -0500

    my files

--- Board_Files/board.cpp
+++ Board_Files/board.cpp
@@ -3,12 +3,7 @@
 #include <iostream>
 
 Board::Board(){
-    BoardCharacter("Miss Scarlett", 3);
-    BoardCharacter("Professor Plum", 5);
-    BoardCharacter("Col Mustard", 7);
-    BoardCharacter("Mrs. Peacock", 13);
-    BoardCharacter("Mr. Green", 17);
-    BoardCharacter("Mrs. White", 19);
+    
 }
 
 void Board::print(){
